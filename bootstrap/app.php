99b58628bd4bb15dbbfe4eb8a2411816e3e48dbc
Deyand03 <defryyandy7@gmail.com>
2025-12-08 01:19:10 +0700

    shutdown csrf token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware): void
+    {
+        $middleware->validateCsrfTokens(except: [
+            'api/*',       // Matikan CSRF untuk semua route yang berawalan /api
+            'login',       // Matikan untuk route login spesifik (jaga-jaga)
+            'logout',
+            'register'
+        ]);
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
